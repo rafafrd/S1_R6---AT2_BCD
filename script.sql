37fef480759d59e5bf8f44fbf1c4cd8f316873dd
rafafrd <bolekao03@gmail.com>
2025-11-26 09:19:15 -0300

    feat: event

--- script.sql
+++ script.sql
@@ -4,8 +4,7 @@ USE tecmarket;
 -- Tabela CARGO
 CREATE TABLE IF NOT EXISTS cargo (
     id_cargo INT PRIMARY KEY AUTO_INCREMENT,
-    dc_cargo VARCHAR(70) NOT NULL,
-    id_user INT
+    dc_cargo VARCHAR(70) NOT NULL
 );
 
 -- Tabela USUARIOS
@@ -19,11 +18,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     id_cargo INT,
     FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo)
 );
-
--- Adiciona a FK pq tava criando dependencia
-ALTER TABLE cargo
-ADD CONSTRAINT fk_cargo_usuario
-FOREIGN KEY (id_user) REFERENCES usuarios(id_user);
 
 -- Tabela TELEFONE
 CREATE TABLE IF NOT EXISTS telefone (
@@ -79,13 +73,91 @@ CREATE TABLE IF NOT EXISTS fornecedor (
 );
 
 
+-- Procedures
+-- ==============================================
+-- 1 - Crie um Stored Procedure para cadastrar produtos;
+DELIMITER $$
+CREATE PROCEDURE CadastrarProduto (
+    IN p_dc_produto VARCHAR(100),
+    IN p_descricao TEXT,
+    IN p_vl_produto DECIMAL(10, 2),
+    IN p_qntd_estoque INT
+)
+BEGIN
+    INSERT INTO produto (dc_produto, descricao, vl_produto, qntd_estoque)
+    VALUES (p_dc_produto, p_descricao, p_vl_produto, p_qntd_estoque);
+END $$
+DELIMITER ;
+
+-- 2 - Crie um Stored Procedure para gerar relatório de vendas por data de início e fim;
+DELIMITER $$
+CREATE PROCEDURE RelatorioVendasPorData (
+    IN p_data_inicio DATE,
+    IN p_data_fim DATE
+)
+BEGIN
+    SELECT p.id_pedido, p.dt_pedido, u.nome AS cliente, p.vl_total
+    FROM pedido p
+    JOIN usuarios u ON p.id_user = u.id_user
+    WHERE DATE(p.dt_pedido) BETWEEN p_data_inicio AND p_data_fim;
+END $$
+DELIMITER ;
+
+-- 3 - Crie um função para calcular o valor total de cada venda;
+DELIMITER $$
+CREATE FUNCTION CalcularValorTotalVenda (
+    p_id_pedido INT
+) 
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    DECLARE total DECIMAL(10, 2);
+
+    -- Correção: Usar INTO para atribuir o valor à variável
+    SELECT SUM(vl_item_produto) INTO total
+    FROM itens_pedido
+    WHERE id_pedido = p_id_pedido;
+    
+    -- Retorna 0 se for nulo (caso não tenha itens)
+    RETURN COALESCE(total, 0.00);
+END $$
+DELIMITER ;
+
+-- 5 - Crie um trigger para atualizar o estoque quando um item de venda for inserido;
+
+DELIMITER $$
+	CREATE TRIGGER trg_atualiza_estoque
+    AFTER INSERT ON itens_pedido
+    FOR EACH ROW
+    BEGIN
+		SET qntd_estoque = qntd_estoque - NEW.qntd_produto;
+    END $$
+DELIMITER ;
+
+-- 6 - Crie um evento (EVENT SCHEDULER) que gere diariamente uma tabela de log de estoque;
+
+-- CREATE EVENT [IF NOT EXISTS] event_name
+--     ON SCHEDULE schedule
+--     [ON COMPLETION [NOT] PRESERVE]
+--     [ENABLE | DISABLE | DISABLE ON SLAVE]
+--     DO
+--         event_body;
+
+
+
+
+
+
+
+
+
 -- popular tabelas
 
 -- add cargo com null primeiro para evitar erro de fk
-INSERT INTO cargo (dc_cargo, id_user) VALUES
-('Atendente', NULL), -- id 1
-('Gerente', NULL),   -- id 2
-('Caixa', NULL);     -- id 3
+INSERT INTO cargo (dc_cargo) VALUES
+('Atendente'), -- id 1
+('Gerente'),   -- id 2
+('Caixa');     -- id 3
 
 -- add users (referenciando os cargos criados)
 INSERT INTO usuarios (nome, rua, numero, bairro, CEP, id_cargo) VALUES
@@ -96,10 +168,11 @@ INSERT INTO usuarios (nome, rua, numero, bairro, CEP, id_cargo) VALUES
 ('Eva Costa', 'Rua E', '202', 'Bairro V', '56789012', NULL), -- Cliente
 ('Fábio Lima', 'Rua F', '303', 'Bairro U', '67890123', 3); -- Caixa
 
--- att a tabela CARGO para dizer quem ocupa aquele cargo
-UPDATE cargo SET id_user = 3 WHERE id_cargo = 1; -- Atendente
-UPDATE cargo SET id_user = 4 WHERE id_cargo = 2; -- Gerente
-UPDATE cargo SET id_user = 6 WHERE id_cargo = 3; -- Caixa
+SELECT nome,
+    COALESCE(dc_cargo,"Cliente") AS cargo
+FROM usuarios
+LEFT JOIN cargo 
+ON usuarios.id_cargo = cargo.id_cargo;
 
 -- Telefones
 INSERT INTO telefone (telefone, id_user) VALUES
@@ -109,16 +182,16 @@ INSERT INTO telefone (telefone, id_user) VALUES
 ('41954321098', 4),
 ('51943210987', 5);
 
+-- Produtos - tem que vir antes para nao bugar
+INSERT INTO produto (dc_produto, descricao, vl_produto, qntd_estoque) VALUES
+('Notebook Gamer', 'Notebook potente', 5500.00, 10),    -- id 1
+('Smartphone Azul', 'Câmera alta resolução', 1500.00, 25), -- id 2
+('Tablet DEF', 'Tablet leve', 1200.00, 15),           -- id 3
+('Monitor GHI', 'Monitor 4K', 2000.00, 8);            -- id 4
+
 -- Categorias e Fornecedores
 INSERT INTO categoria (dc_categoria) VALUES
 ('Eletrônicos'), ('Celulares'), ('Tablets'), ('Monitores');
--- Produtos - tem que vir antes para nao bugar
-INSERT INTO produto (dc_produto, descricao, vl_produto, qntd_estoque, id_categoria) VALUES
-('Notebook Gamer XYZ', 'Notebook potente para jogos e tarefas pesadas.', 5500.00, 10, 1), -- id 1
-('Smartphone ABC', 'Smartphone com câmera de alta resolução e desempenho rápido.', 1500.00, 25, 2), -- id 2
-('Tablet DEF', 'Tablet leve e versátil para trabalho e entretenimento.', 1200.00, 15, 3), -- id 3
-('Monitor 4K GHI', 'Monitor com resolução 4K para imagens nítidas e detalhadas.', 2000.00, 8, 4); -- id 4
-
 
 INSERT INTO fornecedor (dc_fornecedor, id_produto) VALUES
 ('Tech Distributors Inc.', 1), ('Gadget World', 2), ('Tablet Suppliers Co.', 3), ('Display Masters Ltd.', 4);
@@ -137,89 +210,3 @@ INSERT INTO itens_pedido (id_pedido, id_produto, qntd_produto, vl_item_produto) 
 (3, 1, 1, 5500.00),
 (3, 4, 1, 2000.00),
 (4, 3, 2, 2400.00);
-
-
--- Procedures
--- ==============================================
-DELIMITER $$
-CREATE PROCEDURE CadastrarProduto (
-    IN p_dc_produto VARCHAR(100),
-    IN p_descricao TEXT,
-    IN p_vl_produto DECIMAL(10, 2),
-    IN p_qntd_estoque INT,
-    IN p_id_categoria INT
-)
-BEGIN
-    INSERT INTO produto (dc_produto, descricao, vl_produto, qntd_estoque, id_categoria)
-    VALUES (p_dc_produto, p_descricao, p_vl_produto, p_qntd_estoque, p_id_categoria);
-END $$
-DELIMITER ;
-
-CALL `CadastrarProduto`('Fone de Ouvido XYZ', 'Fone de ouvido com cancelamento de ruído.', 300.00, 50, 1);
-SELECT * FROM produto;
-
--- Procedure: Relatório Vendas
-DELIMITER $$
-CREATE PROCEDURE RelatorioVendasPorData (
-    IN p_data_inicio DATE,
-    IN p_data_fim DATE
-)
-BEGIN
-    SELECT p.id_pedido, p.dt_pedido, u.nome AS cliente, p.vl_total
-    FROM pedido p
-    JOIN usuarios u ON p.id_user = u.id_user
-    WHERE DATE(p.dt_pedido) BETWEEN p_data_inicio AND p_data_fim;
-END $$
-DELIMITER ;
-CALL RelatorioVendasPorData('2025-11-01', '2025-11-30');
-
--- Function: Calcular Valor Total
-DELIMITER $$
-CREATE FUNCTION CalcularValorTotalVenda (
-    p_id_pedido INT -- id do pedido
-) 
-RETURNS DECIMAL(10, 2)
-DETERMINISTIC
-BEGIN
-    DECLARE total DECIMAL(10, 2);
-
-    -- Correção: Usar INTO para atribuir o valor à variável
-    SELECT SUM(vl_item_produto) INTO total -- soma os produtos
-    FROM itens_pedido
-    WHERE id_pedido = p_id_pedido;
-    
-    -- Retorna 0 se for nulo (caso não tenha itens)
-    RETURN COALESCE(total, 0.00); -- retorna valor total
-END $$
-DELIMITER ;
-
-select CalcularValorTotalVenda(3);
-
--- Crie um função para calcular o valor de desconto de um determinado produto (preço, percentual);
-DELIMITER $$
-CREATE FUNCTION CalcularDescontoProduto (
-    p_vl_produto DECIMAL(10, 2),
-    p_percentual_desconto INT
-)
-RETURNS DECIMAL(10, 2)
-DETERMINISTIC
-BEGIN
-    DECLARE valor_desconto INT;
-    SET valor_desconto = (p_vl_produto * p_percentual_desconto) / 100;
-    RETURN valor_desconto;
-END $$
-DELIMITER ;
-
-SELECT CalcularDescontoProduto(2000.00, 10) AS desconto;
-
--- Crie um trigger para atualizar o esqoque quando um item de venda for inserido;
-DELIMITER $$
-CREATE TRIGGER trg_atualiza_estoque
-AFTER INSERT ON itens_pedido
-FOR EACH ROW
-BEGIN
-    UPDATE produto
-    SET qntd_estoque = qntd_estoque - NEW.qntd_produto
-    WHERE id_produto = NEW.id_produto;
-END $$
-DELIMITER ;
